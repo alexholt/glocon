3a397b718256dd8c0ee1d43950fd74e6825ac7a7
alexholt <alex.m.holt@gmail.com>
2016-11-14 23:00:35 -0800

    Add tank unit

--- src/client/unit_manager.js
+++ src/client/unit_manager.js
@@ -1,19 +1,46 @@
+const SIZE = 40;
+
 let territories = {};
+let tanks = {};
+let tank;
 
 function init(_territories) {
   territories = _territories;
+	tank = document.createElement('section');
+	tank.innerHTML = require('./images/tank.svg');
+  tank.style.position = 'fixed';
+  tank.style.width = SIZE + 'px';
+  tank.style.height = SIZE + 'px';
+  tank.style.pointerEvents = 'none';
 }
 
 function draw(context, scale, offsetX, offsetY) {
-  context.save();
-  context.fillStyle = 'yellow';
   Object.keys(territories).forEach((territory) => {
     let { x, y, x2, y2 } = territories[territory].getBoundingBox();
     x = (x2 - x) / 2 + x;
     y = (y2 - y) / 2 + y;
-    context.fillRect((x - offsetX) / scale, (y - offsetY) / scale, 10, 10);
+    x = (x - offsetX) / scale - SIZE / 2;
+    y = (y - offsetY) / scale - SIZE / 2;
+    let unit;
+    if (!tanks[territory]) {
+      unit = tanks[territory] = tank.cloneNode(true);
+      document.querySelector('main').appendChild(unit);
+    } else {
+      unit = tanks[territory];
+    }
+
+    if (unit.style.left !== x + 'px') {
+      unit.style.left = x + 'px';
+    }
+    if (unit.style.top !== y + 'px') {
+      unit.style.top = y + 'px';
+    }
+    //context.fillStyle = 'yellow';
+    //context.fillRect(x, y, SIZE, SIZE);
+    //context.fillStyle = 'black';
+    //const { width } = context.measureText('10');
+    //context.fillText( '10', x + width / 2, y + SIZE / 2 + 5);
   });
-  context.restore();
 }
 
 export default {
--- test/territory.js
+++ test/territory.js
@@ -26,16 +26,16 @@ describe('Territory', () => {
       const subject = new Territory('m 10 10 m 110 100 m 110 110 m 100 110 z');
       expect(subject.getBoundingBox()).to.have.property('x', 10);
       expect(subject.getBoundingBox()).to.have.property('y', 10);
-      expect(subject.getBoundingBox()).to.have.property('x2', 110);
-      expect(subject.getBoundingBox()).to.have.property('y2', 110);
+      expect(subject.getBoundingBox()).to.have.property('x2', 330);
+      expect(subject.getBoundingBox()).to.have.property('y2', 330);
     });
 
     it('should work for a simple rectangle with commas', () => {
       const subject = new Territory('m 10,10 100,0 0,100 -100,0 z');
       expect(subject.getBoundingBox()).to.have.property('x', 10);
       expect(subject.getBoundingBox()).to.have.property('y', 10);
-      expect(subject.getBoundingBox()).to.have.property('x2', 100);
-      expect(subject.getBoundingBox()).to.have.property('y2', 100);
+      expect(subject.getBoundingBox()).to.have.property('x2', 110);
+      expect(subject.getBoundingBox()).to.have.property('y2', 110);
     });
 
   });
